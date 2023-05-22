c25de6a6a88432ad13902ca9494a15129a871b73
Ridoy Sharif <developersridoy@gmail.com>
2023-05-22 23:00:55 +0600

    Banner section design

--- src/components/shared/header/Header.jsx
+++ src/components/shared/header/Header.jsx
@@ -1,8 +1,50 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
+
+  const navMenu =<>
+      <li><Link className='text-lg font-medium' to={`/`}>Home</Link></li>
+      <li><Link className='text-lg font-medium' to={`/`}>Our Menu</Link></li>
+      <li><Link className='text-lg font-medium' to={`/`}>Our Shop</Link></li>
+      <li><Link className='text-lg font-medium' to={`/`}>Contact Us</Link></li>
+      <li><Link className='text-lg font-medium' to={`/`}>Dashboard</Link></li>
+  </>
+
   return (
-    <div>Header</div>
+    <div>
+      <div className="navbar fixed z-10 bg-opacity-30 bg-base-100 text-white px-10">
+          <div className="navbar-start">
+            <div className="dropdown">
+              <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+                </svg>
+              </label>
+              <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                  {navMenu}
+              </ul>
+            </div>
+            <Link>
+                <h3 className='text-3xl font-semibold'>BISTRO BOSS</h3>
+                <p className='text-2xl font-medium'>Restaurant</p>
+            </Link>
+          </div>
+          {/* <div className="navbar-center ">
+            <ul className="menu menu-horizontal px-1">
+                {navMenu}
+            </ul>
+          </div> */}
+          {/* <div className="navbar-end">
+            <a className="btn">Get started</a>
+          </div> */}
+          <div className="navbar-end hidden lg:flex">
+              <ul className="menu menu-horizontal px-1">
+                  {navMenu}
+              </ul>
+          </div>
+        </div>
+    </div>
   )
 }
 
---
+++ src/pages/home/banner/Banner.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
+// image import
+import slider1 from '../../../assets/home/01.jpg'
+import slider2 from '../../../assets/home/02.jpg'
+import slider3 from '../../../assets/home/03.png'
+import slider4 from '../../../assets/home/04.jpg'
+import slider5 from '../../../assets/home/05.png'
+import slider6 from '../../../assets/home/06.png'
+
+const Banner = () => {
+  return (
+    <Carousel className='text-center'>
+        <div>
+            <img src={slider1} alt='Slider'/>
+        </div>
+        <div>
+            <img src={slider2} alt='Slider'/>
+        </div>
+        <div>
+            <img src={slider3} alt='Slider'/>
+        </div>
+        <div>
+            <img src={slider4} alt='Slider'/>
+        </div>
+        <div>
+            <img src={slider5} alt='Slider'/>
+        </div>
+        <div>
+            <img src={slider6} alt='Slider'/>
+        </div>
+    </Carousel>
+  )
+}
+
+export default Banner
