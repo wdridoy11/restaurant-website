98ded4b38923403e3a270964758c50706389728d
Ridoy Sharif <developersridoy@gmail.com>
2023-05-23 09:45:17 +0600

    eslint problem

--- src/pages/home/banner/Banner.jsx
+++ src/pages/home/banner/Banner.jsx
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 
