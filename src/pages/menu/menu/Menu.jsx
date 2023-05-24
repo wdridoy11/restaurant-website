6033f8154a9b1839483f154ef5bcd6a650921c85
Ridoy Sharif <developersridoy@gmail.com>
2023-05-24 09:42:12 +0600

    cover page Parallax added

--- src/pages/menu/menu/Menu.jsx
+++ src/pages/menu/menu/Menu.jsx
@@ -2,12 +2,18 @@ import React from 'react'
 import { Helmet} from "react-helmet-async";
 import Cover from '../../../components/shared/cover/Cover';
 import menu from '../../../assets/menu/banner3.jpg'
+import PopularMenu from '../../home/popularMenu/PopularMenu';
 const Menu = () => {
   return (
     <div>
         <div>
             <Helmet><title>Bistro | Menu</title></Helmet>
             <Cover coverImg={menu} coverTitle="Our Menu"></Cover>
+            <PopularMenu></PopularMenu>
+            <Cover coverImg={menu} coverTitle="Our Menu"></Cover>
+            <PopularMenu></PopularMenu>
+            <Cover coverImg={menu} coverTitle="Our Menu"></Cover>
+            <PopularMenu></PopularMenu>
             <div>
 
             </div>
